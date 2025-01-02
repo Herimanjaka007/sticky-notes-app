76e0116bc70070eb4951686dee5193f004b54bbb
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2025-01-02 15:55:20 +0300

    implement draggable functionality for note cards

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,17 @@
 import { useRef } from "react";
 import Trash from "../icons/Trash";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const NoteCard = ({ note }) => {
     const body = JSON.parse(note.body);
-    const position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
+    
     const textareaRef = useRef(null);
+    const [position, setPosition] = useState(JSON.parse(note.position));
+
+    const mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
 
     const autoGrow = (textareaRef) => {
         const {current} = textareaRef;
@@ -14,13 +19,43 @@ const NoteCard = ({ note }) => {
         current.style.height = current.scrollHeight + "px";
     };
 
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+    
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    };
+
+    const mouseMove = (e) => {
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+    
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+    
+        setPosition({
+            x: cardRef.current.offsetLeft - mouseMoveDir.x,
+            y: cardRef.current.offsetTop - mouseMoveDir.y,
+        });
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
+
     useEffect(()=> {
         autoGrow(textareaRef);
-    });
+    }, []);
     
     return (
         <div
             className="card"
+            ref={cardRef}
+            onMouseDown={mouseDown}
             style={{
                 backgroundColor: colors.colorBody,
                 left: `${position.x}px`,
